899069c3d8d4bfd2eba45dfe1ab00f70edb9c989
Felix JC <felixjcalvo@gmail.com>
2020-08-06 17:27:30 +0200

    More practice

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+// Install extension "Live server"
+// Right click on htlm file and select "Open with Live Server"
+
 let name = "Felix";
-
-const interestRate = 0.3;
-
+const interestRate = 0.3; // constant
+// interestRate = 1
+console.log(interestRate);
+// -----------------------------
 let firstName = undefined; // 
 let secondName = null; // explicitly clear value
+// -----------------------------
+// Dynamic progrmming language example
+let nombre = "Felix"
+console.log(typeof nombre);
+nombre = 1
+console.log(typeof nombre);
+// -----------------------------
+// Object
+let person = {
+    name: "Felix",
+    age: 27
+};
+console.log(person);
 
-console.log(interestRate);
+person.name = "xileF";
+console.log(person);
+// -----------------------------
+// Array
+let selectedColors = ["red", "blue"];
+console.log(selectedColors);
+console.log(selectedColors[0]);
+
+// Multiple types allowed
+selectedColors.push(99);
+console.log(selectedColors);
